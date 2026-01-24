f1dcfc5c3c760af4b3286c055dab3276ac1b5139
Damoy <Damoya@proton.me>
2026-01-25 00:11:20 +0800

    实现 Dormand-Prince 5(4) 积分器

--- cuda/kerr_trace.cuh
+++ cuda/kerr_trace.cuh
@@ -46,16 +46,17 @@ __device__ float3 trace_ray(
     float transmittance = 1.0f;
     float h = CONFIG_INTEGRATOR_INITIAL_STEP;
     const float tol = CONFIG_INTEGRATOR_TOLERANCE;
-    static const float a21 = 0.2f;
+    static const float a21 = 1.0f / 5.0f;
     static const float a31 = 3.0f / 40.0f, a32 = 9.0f / 40.0f;
-    static const float a41 = 0.3f, a42 = -0.9f, a43 = 1.2f;
-    static const float a51 = -11.0f / 54.0f, a52 = 2.5f, a53 = -70.0f / 27.0f, a54 = 35.0f / 27.0f;
-    static const float a61 = 1631.0f / 55296.0f, a62 = 175.0f / 512.0f, a63 = 575.0f / 13824.0f, a64 = 44275.0f / 110592.0f, a65 = 253.0f / 4096.0f;
-    static const float b1 = 37.0f / 378.0f, b3 = 250.0f / 621.0f, b4 = 125.0f / 594.0f, b6 = 512.0f / 1771.0f;
-    static const float dc1 = 37.0f / 378.0f - 2825.0f / 27648.0f, dc3 = 250.0f / 621.0f - 18575.0f / 48384.0f, dc4 = 125.0f / 594.0f - 13525.0f / 55296.0f, dc5 = -277.0f / 14336.0f, dc6 = 512.0f / 1771.0f - 0.25f;
+    static const float a41 = 44.0f / 45.0f, a42 = -56.0f / 15.0f, a43 = 32.0f / 9.0f;
+    static const float a51 = 19372.0f / 6561.0f, a52 = -25360.0f / 2187.0f, a53 = 64448.0f / 6561.0f, a54 = -212.0f / 729.0f;
+    static const float a61 = 9017.0f / 3168.0f, a62 = -355.0f / 33.0f, a63 = 46732.0f / 5247.0f, a64 = 49.0f / 176.0f, a65 = -5103.0f / 18656.0f;
+    static const float a71 = 35.0f / 384.0f, a73 = 500.0f / 1113.0f, a74 = 125.0f / 192.0f, a75 = -2187.0f / 6784.0f, a76 = 11.0f / 84.0f;
+    static const float b1 = 35.0f / 384.0f, b3 = 500.0f / 1113.0f, b4 = 125.0f / 192.0f, b5 = -2187.0f / 6784.0f, b6 = 11.0f / 84.0f;
+    static const float dc1 = 35.0f / 384.0f - 5179.0f / 57600.0f, dc3 = 500.0f / 1113.0f - 7571.0f / 16695.0f, dc4 = 125.0f / 192.0f - 393.0f / 640.0f, dc5 = -2187.0f / 6784.0f + 92097.0f / 339200.0f, dc6 = 11.0f / 84.0f - 187.0f / 2100.0f, dc7 = -1.0f / 40.0f;
     for (int i = 0; i < CONFIG_INTEGRATOR_MAX_STEPS && transmittance > CONFIG_TRANSMITTANCE_CUTOFF; i++)
     {
-        RayDerivs k1, k2, k3, k4, k5, k6;
+        RayDerivs k1, k2, k3, k4, k5, k6, k7;
         RayState next_s;
         float error;
         int attempts = 0;
@@ -68,25 +69,26 @@ __device__ float3 trace_ray(
             k4 = get_derivs({s.x + h * (a41 * k1.dx + a42 * k2.dx + a43 * k3.dx), s.y + h * (a41 * k1.dy + a42 * k2.dy + a43 * k3.dy), s.z + h * (a41 * k1.dz + a42 * k2.dz + a43 * k3.dz), s.px + h * (a41 * k1.dpx + a42 * k2.dpx + a43 * k3.dpx), s.py + h * (a41 * k1.dpy + a42 * k2.dpy + a43 * k3.dpy), s.pz + h * (a41 * k1.dpz + a42 * k2.dpz + a43 * k3.dpz)}, pt);
             k5 = get_derivs({s.x + h * (a51 * k1.dx + a52 * k2.dx + a53 * k3.dx + a54 * k4.dx), s.y + h * (a51 * k1.dy + a52 * k2.dy + a53 * k3.dy + a54 * k4.dy), s.z + h * (a51 * k1.dz + a52 * k2.dz + a53 * k3.dz + a54 * k4.dz), s.px + h * (a51 * k1.dpx + a52 * k2.dpx + a53 * k3.dpx + a54 * k4.dpx), s.py + h * (a51 * k1.dpy + a52 * k2.dpy + a53 * k3.dpy + a54 * k4.dpy), s.pz + h * (a51 * k1.dpz + a52 * k2.dpz + a53 * k3.dpz + a54 * k4.dpz)}, pt);
             k6 = get_derivs({s.x + h * (a61 * k1.dx + a62 * k2.dx + a63 * k3.dx + a64 * k4.dx + a65 * k5.dx), s.y + h * (a61 * k1.dy + a62 * k2.dy + a63 * k3.dy + a64 * k4.dy + a65 * k5.dy), s.z + h * (a61 * k1.dz + a62 * k2.dz + a63 * k3.dz + a64 * k4.dz + a65 * k5.dz), s.px + h * (a61 * k1.dpx + a62 * k2.dpx + a63 * k3.dpx + a64 * k4.dpx + a65 * k5.dpx), s.py + h * (a61 * k1.dpy + a62 * k2.dpy + a63 * k3.dpy + a64 * k4.dpy + a65 * k5.dpy), s.pz + h * (a61 * k1.dpz + a62 * k2.dpz + a63 * k3.dpz + a64 * k4.dpz + a65 * k5.dpz)}, pt);
+            k7 = get_derivs({s.x + h * (a71 * k1.dx + a73 * k3.dx + a74 * k4.dx + a75 * k5.dx + a76 * k6.dx), s.y + h * (a71 * k1.dy + a73 * k3.dy + a74 * k4.dy + a75 * k5.dy + a76 * k6.dy), s.z + h * (a71 * k1.dz + a73 * k3.dz + a74 * k4.dz + a75 * k5.dz + a76 * k6.dz), s.px + h * (a71 * k1.dpx + a73 * k3.dpx + a74 * k4.dpx + a75 * k5.dpx + a76 * k6.dpx), s.py + h * (a71 * k1.dpy + a73 * k3.dpy + a74 * k4.dpy + a75 * k5.dpy + a76 * k6.dpy), s.pz + h * (a71 * k1.dpz + a73 * k3.dpz + a74 * k4.dpz + a75 * k5.dpz + a76 * k6.dpz)}, pt);
             next_s = {
-                s.x + h * (b1 * k1.dx + b3 * k3.dx + b4 * k4.dx + b6 * k6.dx),
-                s.y + h * (b1 * k1.dy + b3 * k3.dy + b4 * k4.dy + b6 * k6.dy),
-                s.z + h * (b1 * k1.dz + b3 * k3.dz + b4 * k4.dz + b6 * k6.dz),
-                s.px + h * (b1 * k1.dpx + b3 * k3.dpx + b4 * k4.dpx + b6 * k6.dpx),
-                s.py + h * (b1 * k1.dpy + b3 * k3.dpy + b4 * k4.dpy + b6 * k6.dpy),
-                s.pz + h * (b1 * k1.dpz + b3 * k3.dpz + b4 * k4.dpz + b6 * k6.dpz)};
-            float scale_x = tol * fmaxf(fabsf(s.x), 1.0f);
-            float scale_y = tol * fmaxf(fabsf(s.y), 1.0f);
-            float scale_z = tol * fmaxf(fabsf(s.z), 1.0f);
-            float scale_px = tol * fmaxf(fabsf(s.px), 1.0f);
-            float scale_py = tol * fmaxf(fabsf(s.py), 1.0f);
-            float scale_pz = tol * fmaxf(fabsf(s.pz), 1.0f);
-            float err_x = fabsf(h * (dc1 * k1.dx + dc3 * k3.dx + dc4 * k4.dx + dc5 * k5.dx + dc6 * k6.dx)) / scale_x;
-            float err_y = fabsf(h * (dc1 * k1.dy + dc3 * k3.dy + dc4 * k4.dy + dc5 * k5.dy + dc6 * k6.dy)) / scale_y;
-            float err_z = fabsf(h * (dc1 * k1.dz + dc3 * k3.dz + dc4 * k4.dz + dc5 * k5.dz + dc6 * k6.dz)) / scale_z;
-            float err_px = fabsf(h * (dc1 * k1.dpx + dc3 * k3.dpx + dc4 * k4.dpx + dc5 * k5.dpx + dc6 * k6.dpx)) / scale_px;
-            float err_py = fabsf(h * (dc1 * k1.dpy + dc3 * k3.dpy + dc4 * k4.dpy + dc5 * k5.dpy + dc6 * k6.dpy)) / scale_py;
-            float err_pz = fabsf(h * (dc1 * k1.dpz + dc3 * k3.dpz + dc4 * k4.dpz + dc5 * k5.dpz + dc6 * k6.dpz)) / scale_pz;
+                s.x + h * (b1 * k1.dx + b3 * k3.dx + b4 * k4.dx + b5 * k5.dx + b6 * k6.dx),
+                s.y + h * (b1 * k1.dy + b3 * k3.dy + b4 * k4.dy + b5 * k5.dy + b6 * k6.dy),
+                s.z + h * (b1 * k1.dz + b3 * k3.dz + b4 * k4.dz + b5 * k5.dz + b6 * k6.dz),
+                s.px + h * (b1 * k1.dpx + b3 * k3.dpx + b4 * k4.dpx + b5 * k5.dpx + b6 * k6.dpx),
+                s.py + h * (b1 * k1.dpy + b3 * k3.dpy + b4 * k4.dpy + b5 * k5.dpy + b6 * k6.dpy),
+                s.pz + h * (b1 * k1.dpz + b3 * k3.dpz + b4 * k4.dpz + b5 * k5.dpz + b6 * k6.dpz)};
+            float scale_x = tol * fabsf(s.x);
+            float scale_y = tol * fabsf(s.y);
+            float scale_z = tol * fabsf(s.z);
+            float scale_px = tol * fabsf(s.px);
+            float scale_py = tol * fabsf(s.py);
+            float scale_pz = tol * fabsf(s.pz);
+            float err_x = fabsf(h * (dc1 * k1.dx + dc3 * k3.dx + dc4 * k4.dx + dc5 * k5.dx + dc6 * k6.dx + dc7 * k7.dx)) / scale_x;
+            float err_y = fabsf(h * (dc1 * k1.dy + dc3 * k3.dy + dc4 * k4.dy + dc5 * k5.dy + dc6 * k6.dy + dc7 * k7.dy)) / scale_y;
+            float err_z = fabsf(h * (dc1 * k1.dz + dc3 * k3.dz + dc4 * k4.dz + dc5 * k5.dz + dc6 * k6.dz + dc7 * k7.dz)) / scale_z;
+            float err_px = fabsf(h * (dc1 * k1.dpx + dc3 * k3.dpx + dc4 * k4.dpx + dc5 * k5.dpx + dc6 * k6.dpx + dc7 * k7.dpx)) / scale_px;
+            float err_py = fabsf(h * (dc1 * k1.dpy + dc3 * k3.dpy + dc4 * k4.dpy + dc5 * k5.dpy + dc6 * k6.dpy + dc7 * k7.dpy)) / scale_py;
+            float err_pz = fabsf(h * (dc1 * k1.dpz + dc3 * k3.dpz + dc4 * k4.dpz + dc5 * k5.dpz + dc6 * k6.dpz + dc7 * k7.dpz)) / scale_pz;
             error = fmaxf(fmaxf(fmaxf(err_x, err_y), fmaxf(err_z, err_px)), fmaxf(err_py, err_pz));
             if (error <= 1.0f)
                 accepted = true;
